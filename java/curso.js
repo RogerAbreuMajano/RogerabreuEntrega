0d81d63bd74559c53401b56fac3b3f51abedd940
Roger Abreu Majano <rogerabreumajano@gmail.com>
2023-03-04 17:37:54 +0100

    first commit

---
+++ java/curso.js
@@ -0,0 +1,18 @@
+
+
+
+/* if else*/
+
+let nombre = prompt("nombre");
+let edad = prompt("edad");
+let sexo = prompt("indica tu sexo");
+let estadoCivil = prompt("Indique su estado civil");
+
+if (edad >= 54) {
+    console.log(`Hola ${nombre}`);
+    let ciudad = prompt("Indicar la ciudad donde desea viajar")
+} else if (edad < 54) {
+    alert("usted no cumple la edad requerida para los viajes de personas mayores ")
+    
+}
+
